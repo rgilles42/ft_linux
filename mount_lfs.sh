67ec8ebef65ea3f08f0f944cfbf280e327657254
Raphaël Gilles <rgilles@student.42.fr>
2022-11-26 00:51:18 +0100

    finished Ch8

--- mount_lfs.sh
+++ mount_lfs.sh
@@ -3,3 +3,9 @@ echo Mounting /dev/nbd0 on $LFS
 sudo mount -v -t ext4 /dev/nbd0p2 $LFS
 sudo mount -v -t ext4 /dev/nbd0p1 $LFS/boot
 sudo mount -v -t ext4 /dev/nbd0p3 $LFS/home
+
+sudo mount -v --bind /dev $LFS/dev
+sudo mount -v --bind /dev/pts $LFS/dev/pts
+sudo mount -vt proc proc $LFS/proc
+sudo mount -vt sysfs sysfs $LFS/sys
+sudo mount -vt tmpfs tmpfs $LFS/run
